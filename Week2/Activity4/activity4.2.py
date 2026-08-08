80f2fb9159aeb9c03ef672eb650cc6d2c6443fb9
lahirutm <lahirutm@gmail.com>
2026-08-09 09:51:34 +1200

    activity4.2

---
+++ Week2/Activity4/activity4.2.py
@@ -0,0 +1,11 @@
+def main():
+    my_list = [1,3,5,7,9,11]
+
+    my_list[3:3] = [6,6,6,6,6,6]
+
+    print(my_list)
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ Week2/Activity4/activity4.py
@@ -0,0 +1,9 @@
+def main():
+    my_list = [1,3,5,7,9,11]
+    my_list[2:4] = [-3,-9,-11,-13]
+    print(my_list)
+
+if __name__ == "__main__":
+    main()
+
+
