4b51d06bca6433035a39970f26441f775e1fa0d7
lahirutm <lahirutm@gmail.com>
2026-08-15 09:34:55 +1200

    week3 activity 1

--- Week3/Activity1/uci_data_set.py
+++ Week3/Activity1/uci_data_set.py
@@ -9,10 +9,10 @@ X = iris.data.features
 y = iris.data.targets 
   
 # metadata 
-print(iris.metadata) 
+# print(iris.metadata) 
   
 # variable information 
-print(iris.variables) 
+# print(iris.variables) 
 
 file = pd.read_csv('https://archive.ics.uci.edu/static/public/53/data.csv')
 
@@ -21,14 +21,13 @@ unique_names = set()
 
 for index, row in file.iterrows():
     # Access specific columns using the column name
-    # print(f"Row Index: {index}")
-    # print(row)
-
     unique_names.add(row['class'])
-    
+    # to record number of lines
     no_of_lines = no_of_lines + 1
 
+# print number of lines
 print("\nTotal no. of lines: " + str(no_of_lines))
-
-print(f"Found {len(unique_names)} unique names in the first column:")
+# print number of unique class
+print(f"Found {len(unique_names)} unique names")
+# print all unique names
 print(list(unique_names))
