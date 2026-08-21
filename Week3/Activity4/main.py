f6c7a30524ecf7847506e692f0a8c907fc908996
lahirutm <lahirutm@gmail.com>
2026-08-21 16:46:39 +1200

    Week 3 Activity 4

--- Week3/Activity4/main.py
+++ Week3/Activity4/main.py
@@ -1,6 +1,10 @@
+# Queries for creating database and tables
 from database import create_tables
+# Functions and SQL queries for Students table CRUD
 from student_manager import add_students, view_students, search_student, delete_student
+# Functions and SQL queries for Subjects table CRUD
 from subject_manager import add_subjects, view_subjects, search_subject, delete_subject
+# Functions SQL queries for Lectures table CRUD
 from lecturer_manager import add_lecturers, view_lecturers, search_lecturer, delete_lecturer
 
 def menu():
@@ -33,66 +37,67 @@ def main():
     menu()
     while True:
         choice = input("Select an option: ")
-        if choice == '1':
+        
+        if choice == '1': # Create Student
             f_name = input("Enter first name: ")
             l_name = input("Enter last name: ")
             birth_date = input("Enter birth date: ")
             email = input("Enter email: ")
             add_students(f_name, l_name, birth_date, email)
-        elif choice == '2':
+        elif choice == '2': # View Students
             users = view_students()
             for user in users:
                 print(user)
-        elif choice == '3':
+        elif choice == '3': # Search Student
             name = input("Enter name to search: ")
             students = search_student(name)
             for student in students:
                 print(student)
-        elif choice == '4':
+        elif choice == '4': # Delete Student
             student_id = int(input("Enter student ID to delete: "))
             delete_student(student_id)
 
 
-        elif choice == '5':
+        elif choice == '5': # Create Subject
             s_code = input("Enter subject code: ")
             s_name = input("Enter subject name: ")
             s_udsc = input("Enter udsc: ")
             add_subjects(s_code, s_name, s_udsc)
-        elif choice == '6':
+        elif choice == '6': # View Subject
             subjects = view_subjects()
             for subject in subjects:
                 print(subject)
-        elif choice == '7':
+        elif choice == '7': # Search Subject
             name = input("Enter subject name to search: ")
             subjects = search_subject(name)
             for subject in subjects:
                 print(subject)
-        elif choice == '8':
+        elif choice == '8': # Create Subject
             subject_id = int(input("Enter subject ID to delete: "))
             delete_subject(subject_id)
 
 
-        elif choice == '9':
+        elif choice == '9': # Create Lecturer
             f_name = input("Enter first name: ")
             l_name = input("Enter last name: ")
             address = input("Enter address: ")
             email = input("Enter email: ")
             add_lecturers(f_name, l_name, address, email)
-        elif choice == '10':
+        elif choice == '10': # View Lecturer
             lecturers = view_lecturers()
             for lecturer in lecturers:
                 print(lecturer)
-        elif choice == '11':
+        elif choice == '11': # Search Lecturer
             name = input("Enter lecturer name to search: ")
             lecturers = search_lecturer(name)
             for lecturer in lecturers:
                 print(lecturer)
-        elif choice == '12':
+        elif choice == '12': # Delete Lecturer
             lecturer_id = int(input("Enter lecturer ID to delete: "))
             delete_lecturer(lecturer_id)
 
 
-        elif choice == '99':
+        elif choice == '99': # Exit
             print("Goodbye!")
             break
         else:
